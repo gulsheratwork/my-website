a978008df4f19e204de48989a213eaf41a4b29c9
gulsheratwork <gulsher.atwork@gmail.com>
2024-10-28 23:09:53 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -3,7 +3,7 @@
     const iframe = document.createElement('iframe');
     iframe.src = 'https://gulsheratwork.github.io/my-website/';
     iframe.style.width = '100%';
-    iframe.style.height = '600px';  // Adjust height as needed
+    iframe.style.height = '650px';  // Adjust height as needed
     iframe.style.border = 'none';
 
     // Append iframe to the body
